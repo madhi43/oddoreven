254298e30136ec44a95b050dadbc79b75a8e73c7
sreemathi <sreema8a@gmail.com>
2023-08-30 22:47:39 +0530

    initial commir done

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        calc c = new calc();
+        System.out.println("Enter a number:");
+        int num = i.nextInt();
+        System.out.println(c.func(num));
+    }
+
+    static class calc {
+        String func(int n) {
+            if (n % 2 == 0)
+                return ("Even");
+            else
+                return ("Odd");
+        }
+    }
+}
